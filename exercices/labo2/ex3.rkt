fb9ce24055418ee9ecb0ea37441ae10636a67296
Simon Lalonde <simlalonde@hotmail.com>
2023-09-14 15:12:57 -0400

    rm trace + update with proper constant list

--- exercices/labo2/ex3.rkt
+++ exercices/labo2/ex3.rkt
@@ -1,12 +1,12 @@
 #lang racket
-(require racket/trace)
+; (require racket/trace)
 
 ; Recursive func to get last num of list
 (define (dernier ma-liste)
     (if (null? (cdr ma-liste))
         (car ma-liste)
         (dernier (cdr ma-liste))))
-(trace dernier)
-(define Lettres `(e (f) g))
+; (trace dernier)
+(define Lettres `(a b (c d) (e (f) g)))
 (dernier Lettres)
-(untrace dernier)
+; (untrace dernier)
