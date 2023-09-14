6be2bb34c75a1543e3688ba07901e60a3aba4385
Simon Lalonde <simlalonde@hotmail.com>
2023-09-14 15:09:09 -0400

    ex3 recursive last ele of list

---
+++ exercices/labo2/ex3.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(require racket/trace)
+
+; Recursive func to get last num of list
+(define (dernier ma-liste)
+    (if (null? (cdr ma-liste))
+        (car ma-liste)
+        (dernier (cdr ma-liste))))
+(trace dernier)
+(define Lettres `(e (f) g))
+(dernier Lettres)
+(untrace dernier)
