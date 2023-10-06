9a32795c4d13a991ca997bd453a140d3cf58a8b1
Simon Lalonde <simlalonde@hotmail.com>
2023-10-05 20:28:02 -0400

    Base commit tp1

---
+++ tps/tp1/main.rkt
@@ -0,0 +1,74 @@
+#lang racket
+(provide calculer)
+(provide prefixee)
+
+(provide creer-formule)
+(provide nombre?)
+(provide variable?)
+(provide operateur)
+(provide operande1)
+(provide operande2)
+
+(provide creer-env)
+(provide lier)
+(provide valeur)
+(provide definie?)
+
+; calculer : formule env -> nombre
+; calcule la valeur de la formule en utilisant les valeurs dans l'environnement
+(define (calculer formule env)
+    (formule env))
+
+; retourne une formule prefixee a partir d'une formule quelconque
+(define (prefixee formule)
+    (formule))
+
+; creer une formule a partir d'une expression infixe
+(define (creer-formule exp-infixe)
+    (exp-infixe))
+
+; verifie si la formule est un nombre
+(define (nombre? formule)
+    (number? formule))
+
+; verifie si la formule est une variable
+(define (variable? formule)
+    (symbol? formule))
+
+; Representation sous arbre binaire (noeud=operateur, feuille=operande)
+
+; retourne l'operateur / noeud d'une formule
+; TODO CHECK FEUILLE OU NOEUD
+(define (operateur formule)
+    (formule))
+
+; retourne l'operande1 / feuille gauche d'une formule
+; TODO CHANGE
+(define (operande1 formule)
+    (formule))
+
+; retourne l'operande2 / feuille droite d'une formule
+; TODO CHANGE
+(define (operande2 formule)
+    (formule))
+
+; creer un environnement vide (a remplir avec liste de paires (variable, valeur))
+; TODO CHECK
+(define (creer-env)
+    (list))
+
+; lier variable a valeur dans l'environnement
+; TODO CHECK
+(define (lier variable valeur env)
+    (variable valeur env))
+
+; retourne la valeur associee a la variable dans l'environnement
+; TODO CHECK
+(define (valeur variable env)
+    (variable env))
+
+; verifie si la variable est definie dans l'environnement
+; TODO CHECK
+(define (definie? variable env)
+    (variable env))
+
